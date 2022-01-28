edafed0ff11622895ac9a333e7be5fce7cb77508
Jose A. Álvarez <infoiguacel@gmail.com>
2022-01-28 17:08:09 +0100

    Colors

---
+++ R/palette.R
@@ -0,0 +1,21 @@
+#' EP colors.
+#' @keywords palette
+#' @export
+#' @examples
+#' c$red[1]
+
+c <- list(gray = c("#444444", "#a4a4a4", "#c3c3c3", "#d8d8d8", "#f2f2f2"),
+          blue = c("#016ca2", "#00bfff", "#7cd6ff", "#a5e6ff", "#d5f4fb"),
+          red = c("#b42d2d", "#ff4141", "#ff7676", "#ffb4b4", "#ffdcdc"),
+          green = c("#009600", "#7ed321", "#b2e57a", "#e5f6d3", "#ecf8de"),
+          orange = c("#ff7700", "#ffa000", "#ffcd79", "#ffe1ae", "#ffe9c3"),
+          man = "#ffd100",
+          woman = "#82288c",
+          psoe = "#ED1C24",
+          psoe = "#ED1C24",
+          pp = "#008BD6",
+          cs = "#FA5000",
+          vox = "#5AC035",
+          up = "#6A2E68",
+          mm = "#53EFA5"
+)
